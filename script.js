261fed5d39051c3bae26064dff75b76f269e9baa
ivanv3lkov <ivanv3lkov@gmail.com>
2020-08-09 19:35:38 +0300

    'Initial'

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-"use strict";
-
 fetch("./currencies.json")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
+        let usd = parseFloat(data.rates.USD.toFixed(4));
+        let aud = parseFloat(data.rates.AUD.toFixed(4));
+        let cad = parseFloat(data.rates.CAD.toFixed(4));
+        let bgn = parseFloat(data.rates.BGN.toFixed(4));
 
-    });
+        let values = [usd, aud, cad, bgn]
+
+        let spans = [];
+        
+    }
+    
+
+
+
+
